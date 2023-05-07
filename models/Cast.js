1316c51ecbd30816f1f56d57e779a79301a3a921
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-07 19:27:17 +0300

    Create Cast model

---
+++ models/Cast.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+
+const Schema = mongoose.Schema
+
+
+const castSchema = Schema({
+    title:{type:String, required:true},
+    description:{type:String, required:true},
+
+})
+
+module.exports  =  mongoose.model('cast', castSchema)

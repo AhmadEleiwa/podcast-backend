b718a380235254c4ab37cf7cac7fe2a1ac635eb2
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-07 20:00:58 +0300

    Undo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(bodyParser.json())
 
 app.use('/api/cast', castRoutes)
-
+// app.use('/api/', userRoutes)
 
 app.use('/', (req, res, next) =>{
     return res.json({test:'test'})

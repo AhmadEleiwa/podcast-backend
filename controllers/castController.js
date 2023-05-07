df1d8e7e542f3205f747db9d0d9c28a80c26900f
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-07 19:35:36 +0300

    Add create Test user midware

--- controllers/castController.js
+++ controllers/castController.js
@@ -1,10 +1,18 @@
+const Cast = require("../models/Cast")
+
 const getCasts = async (req,res,next) =>{ 
 
     return res.json({message:'im just testing around'})
 }
 
 
+const addTestCast = async (req, res ,next)=>{
+    let cast = new Cast({title:"Test cast", description:'Test descritpion'})
+    await cast.save()
+    return res.json({message:'Test cast added checkout'})
+}
 
 
 
 exports.getCasts = getCasts
+exports.addTestCast = addTestCast

dcd99c3d576bd56a245f6652f29833c3e850b8b0
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-07 17:58:28 +0300

    Add main mid ware

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(bodyParser.json())
 app.use('/api/cast', castRoutes)
 // app.use('/api/', userRoutes)
 
-
+app.use('/', (req, res, next) =>{
+    return res.json({test:'test'})
+})
 
 
 app.listen(5000)
